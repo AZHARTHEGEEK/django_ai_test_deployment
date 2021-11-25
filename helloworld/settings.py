84c93f8107c2809075c44b66079d4a44cf0d8562
Muhammad Azhar <azharthegeek@gmail.com>
2021-11-25 19:50:44 +0500

    Modified 1.0.1

--- helloworld/settings.py
+++ helloworld/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-riit=e7o)24#)si(ms@1h=uwbg%(*t4ukdb%hfq5av+#$j_fq!'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['techshoor.herokuapp.com','127.0.0.1']
 
