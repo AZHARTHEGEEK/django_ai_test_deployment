30f330b9e9d71b9321b14d590b96d6446a2e9266
Muhammad Azhar <azharthegeek@gmail.com>
2021-12-17 11:59:35 +0500

    .

---
+++ helloworld/azhar.py
@@ -0,0 +1,29 @@
+#write a division function
+def division(a,b):
+    try:
+        c=a/b
+    except ZeroDivisionError:
+        print("Division by zero is not possible")
+    else:
+        print("Division is",c)
+
+#write a program that take user name and password
+#if user name is azhar and password is 12345
+#then print welcome azhar
+#else print invalid user name or password
+
+
+#write a program that take user name in print hello azhar
+#if user name is azhar then print hello azhar
+#else print invalid user name
+
+#write a program to print hello world in print
+#if user name is azhar then print hello world
+#else print invalid user name
+
+#write a program to print hello 
+
+
+#writ a program that take user name
+
+
